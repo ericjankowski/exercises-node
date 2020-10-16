a08971c61d89c3f55e066e16d5a5025f2bd6a62e
Eric Jankowski <eric.r.jankowski@gmail.com>
2020-10-16 08:49:51 -0400

    async examples

---
+++ 1-novice/012-asynchronous-example/async-closure-example.js
@@ -0,0 +1,13 @@
+function asyncFunction(callback){
+    setTimeout(callback, 200);
+}
+
+let color = 'blue';
+
+(color => {
+    asyncFunction(() => {
+        console.log(`The color is ${color}`);
+    });
+})(color);
+
+color = 'green';

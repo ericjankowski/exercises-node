380bf3d38579ba14acc38dab93d4848c5c564024
Eric Jankowski <eric.r.jankowski@gmail.com>
2020-10-22 10:21:44 -0400

    wired up with a sqlite database

--- 1-novice/017-web-application/index.js
+++ 1-novice/017-web-application/index.js
@@ -1,46 +1,103 @@
-// Version 2
+// Version 3
 // =========
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-const articles = [{title: 'Example'}];
+const Article = require('./db').Article;
+const read = require('node-readability');
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/articles', (req, res, next) => {
-    console.log('Fetching all articles');
-    res.send(articles);
+app.post('/articles', (req, res, next) => {
+    const url = req.body.url;
+
+    read(url, (err, result) => {
+        if (err || !result) res.status(500).send('Error downloading article');
+        Article.create(
+            { title: result.title, content: result.content },
+            (err, article) => {
+                if(err) return next(err);
+                res.send('OK');
+            }
+        );
+    });
 });
 
-app.post('/articles', (req, res, next) => {
-    const article = { title: req.body.title };
-    console.log('Saving new article', article.title);
-    articles.push(article);
-    res.send(article);
+app.get('/articles', (req, res, next) => {
+    Article.all((err, articles) => {
+        if(err) return next(err);
+        res.send(articles);
+    });
 });
 
 app.get('/articles/:id', (req, res, next) => {
-    const id = req.params.id;
-    console.log('Fetching:', id);
-    res.send(articles[id]);
+    const id = request.params.id;
+    Article.find(id, (err, article) => {
+        if(err) return next(err);
+        res.send(article);
+    });
 });
 
-app.delete('/articles/:id', (req, res, next) => {
-    const id = req.params.id;
-    console.log('Deleting:', id);
-    delete articles[id];
-    res.send({message: 'Deleted'});
+app.delete('/articles/:id', (req, res, next) =>{
+    Article.delete(id, (err) => {
+        if (err) return next(err);
+        res.send({ message: 'Deleted' });
+    });
 });
 
 app.listen(app.get('port'), () => {
-    console.log('App started on port', app.get('port'));
+    console.log('Appstarted on port', app.get('port'));
 });
 
 module.exports = app;
+
+// Version 2
+// =========
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+
+
+// const articles = [{title: 'Example'}];
+
+// app.set('port', process.env.PORT || 3000);
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/articles', (req, res, next) => {
+//     console.log('Fetching all articles');
+//     res.send(articles);
+// });
+
+// app.post('/articles', (req, res, next) => {
+//     const article = { title: req.body.title };
+//     console.log('Saving new article', article.title);
+//     articles.push(article);
+//     res.send(article);
+// });
+
+// app.get('/articles/:id', (req, res, next) => {
+//     const id = req.params.id;
+//     console.log('Fetching:', id);
+//     res.send(articles[id]);
+// });
+
+// app.delete('/articles/:id', (req, res, next) => {
+//     const id = req.params.id;
+//     console.log('Deleting:', id);
+//     delete articles[id];
+//     res.send({message: 'Deleted'});
+// });
+
+// app.listen(app.get('port'), () => {
+//     console.log('App started on port', app.get('port'));
+// });
+
+// module.exports = app;
 
 
 // // Version 1

dfde379dce7d49d2a76b67a5607aae79d1902c95
Eric Jankowski <eric.r.jankowski@gmail.com>
2020-10-27 12:02:01 -0400

    first unit testing example complete

---
+++ 1-novice/022-unit-testing-example/todo-test.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const Todo = require('./todo');
+const todo = new Todo();
+let testsCompleted = 0;
+
+function deleteTest(){
+    todo.add('Delete Me');
+    assert.strictEqual(todo.length, 1, '1 item should exist');
+    todo.deleteAll();
+    assert.equal(todo.length, 0, 'No items should exist');
+    testsCompleted++;
+}
+
+function addTest(){ //from a professional perspective, this test really sucks
+    todo.deleteAll();
+    todo.add('Added');
+    assert.notStrictEqual(todo.length, 0, '1 item should exist');
+    testsCompleted++;
+}
+
+function doAsyncTest(cb){
+    todo.doAsync(value => {
+        assert.ok(value, 'Callback should be passed true');
+        testsCompleted++;
+        cb();
+    });
+}
+
+function throwsTest(cb){
+    assert.throws(todo.add, /requires/);
+    testsCompleted++;
+}
+
+deleteTest();
+addTest();
+throwsTest();
+doAsyncTest(() => {
+    console.log(`Completed ${testsCompleted} tests`);
+}); 
